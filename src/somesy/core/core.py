8dffc01e566f81cd8a8cf643efabd28d76f9afb1
Mustafa Soylu <m.soylu@fz-juelich.de>
2023-05-16 15:12:47 +0200

    add .somesy.toml support

--- src/somesy/core/core.py
+++ src/somesy/core/core.py
@@ -10,32 +10,38 @@ def get_project_metadata(path: Path) -> ProjectMetadata:
     """Return project metadata to from different sources as a simple dict. Currently, only pyproject.toml inputs are supported.
 
     Args:
-        path (Path): config source path
+        path (Path): input source path
 
     Returns:
         ProjectMetadata: project metadata input to sync output files
     """
     # load the pyproject.toml file
     if path.suffix == ".toml":
-        try:
-            with open(path, "r") as f:
-                config = load(f)
-        except FileNotFoundError:
-            raise FileNotFoundError("pyproject.toml not found")
+        with open(path, "r") as f:
+            input = load(f)
 
         # load project metadata
-        if (
-            "tool" in config
-            and "somesy" in config["tool"]
-            and "project" in config["tool"]["somesy"]
+        if "somesy" in path.name and "project" in input:
+            metadata = input["project"]
+            try:
+                metadata = metadata.unwrap()
+                return ProjectMetadata(**metadata)
+            except Exception as e:
+                raise ValueError(f"Somesy input validation failed: {e}")
+
+        elif (
+            "pyproject" in path.name
+            and "tool" in input
+            and "somesy" in input["tool"]
+            and "project" in input["tool"]["somesy"]
         ):
-            metadata = config["tool"]["somesy"]["project"]
+            metadata = input["tool"]["somesy"]["project"]
             try:
                 metadata = metadata.unwrap()
                 return ProjectMetadata(**metadata)
             except Exception as e:
                 raise ValueError(f"Somesy input validation failed: {e}")
         else:
-            raise ValueError("Somesy config not found in pyproject.toml file.")
+            raise ValueError("Somesy input not found.")
     else:
-        raise ValueError("Only toml files are supported for reading project metadata.")
+        raise ValueError("Only toml files are supported for reading somesy input.")
