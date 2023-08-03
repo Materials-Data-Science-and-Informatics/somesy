8027caa57a4a24f892c16e9096338279d75630d1
Mustafa Soylu <mohkargan@gmail.com>
2023-08-03 11:44:21 +0200

    update repo input/output types for package.json

--- src/somesy/core/writer.py
+++ src/somesy/core/writer.py
@@ -309,11 +309,11 @@ class ProjectMetadataWriter(ABC):
         self._set_property(self._get_key("homepage"), homepage)
 
     @property
-    def repository(self) -> Optional[str]:
+    def repository(self) -> Optional[Union[str, dict]]:
         """Return the repository url of the project."""
         return self._get_property(self._get_key("repository"))
 
     @repository.setter
-    def repository(self, repository: Optional[str]) -> None:
+    def repository(self, repository: Optional[Union[str, dict]]) -> None:
         """Set the repository url of the project."""
         self._set_property(self._get_key("repository"), repository)
