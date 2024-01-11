50adf7451ac12838425a9fbd8f65933e6a87d2af
Mustafa Soylu <mohkargan@gmail.com>
2024-01-11 11:49:27 +0100

    update codemeta tests

--- tests/output/test_codemeta.py
+++ tests/output/test_codemeta.py
@@ -1,121 +1,44 @@
+from somesy.codemeta import Codemeta
 import json
-import sys
-
-import pytest
-import rdflib
-
-from somesy.cff.writer import CFF
-from somesy.codemeta import update_codemeta
-from somesy.codemeta.utils import (
-    _codemeta_context,
-    _graph_from_cm_dict,
-    _graph_from_cm_file,
-    _localize_codemetapy_context,
-)
-from somesy.core.models import SomesyConfig
-
-cm_dict = {
-    "@context": list(_codemeta_context),
-    "@type": "SoftwareSourceCode",
-    "applicationCategory": "Software Development > Libraries > Application Frameworks",
-    "audience": [
-        {"@type": "Audience", "audienceType": "Developers"},
-    ],
-    "author": [
-        {
-            "@id": "https://orcid.org/0123-4567-8910-1112",
-            "@type": "Person",
-            "familyName": "Doe",
-            "givenName": "Jane",
-        },
-    ],
-    "codeRepository": "https://example.com/my-software",
-    "description": "Amazing software",
-    "identifier": "awesome-tool",
-    "keywords": ["amazing", "software"],
-    "license": "http://spdx.org/licenses/MIT",
-    "name": "awesome-tool",
-    "operatingSystem": "POSIX > Linux",
-    "runtimePlatform": "Python 3",
-    "url": "https://example.com/my-software",
-    "version": "0.1.0",
-}
-num_triples = 20  # empirically observed from successful rdflib parse
 
 
-def test_localized_graph():
-    # no context -> do nothing
-    assert _localize_codemetapy_context({}) == {}
+def test_update_codemeta(somesy_input, tmp_path):
+    codemeta_file = tmp_path / "codemeta.json"
 
-    # unexpected context -> exception
-    with pytest.raises(RuntimeError):
-        _localize_codemetapy_context({"@context": "invalid"})
+    cm = Codemeta(codemeta_file)
 
-    # correct context -> replace
-    ret = _localize_codemetapy_context({"@context": _codemeta_context})
-    assert ret["@context"] != _codemeta_context
+    # firstly, create a codemeta file
+    cm.sync(somesy_input.project)
+    cm.save()
 
+    assert codemeta_file.is_file()
+    dat = open(codemeta_file, "rb").read()
 
-def rdflib_audit_hook(name: str, args) -> None:
-    """An audit hook that blocks access when an attempt is made to open an URL.
+    # second time, no changes but codemeta.json exists -> codemeta.json is the same
+    cm.sync(somesy_input.project)
+    cm.save()
+    assert codemeta_file.is_file()
+    dat2 = open(codemeta_file, "rb").read()
+    assert dat == dat2
 
-    See https://rdflib.readthedocs.io/en/stable/_modules/examples/secure_with_audit.html#audit_hook
-    """
-    if name == "urllib.Request":
-        raise PermissionError("Permission denied for URL")
-    return None
+    # third time, change the project name -> codemeta.json is different
+    somesy_input.project.name = "new_name"
+    cm.sync(somesy_input.project)
+    cm.save()
+    assert codemeta_file.is_file()
+    dat3 = open(codemeta_file, "rb").read()
+    assert dat != dat3
 
-
-def test_graph_from_dict():
-    sys.addaudithook(rdflib_audit_hook)
-
-    # test without localized graph
-    # -> rdflib will attempt network access
-    g = rdflib.Graph()
-    with pytest.raises(PermissionError):
-        g.parse(data=json.dumps(cm_dict), format="json-ld")
-
-    # now use the localized graph with local context
-    # -> no network access, but still able to load triples
-    ret = _graph_from_cm_dict(cm_dict)
-    assert len(ret) == num_triples
-
-
-def test_graph_from_file(tmp_path):
-    filepath = tmp_path / "my_codemeta.json"
-
-    # no file -> None
-    assert _graph_from_cm_file(filepath) is None
-    # create file
-    with open(filepath, "w") as f:
-        json.dump(cm_dict, f)
-    # file exists -> returns correct graph
-    ret = _graph_from_cm_file(filepath)
-    assert len(ret) == num_triples
-
-
-def test_update_codemeta(tmp_path):
-    file_path = tmp_path / "my_codemeta.json"
-
-    cff_path = tmp_path / "CITATION.cff"
-    cff = CFF(cff_path)
-    cff.save()
-    assert cff_path.is_file()
-
-    # first time, no file -> codemeta.json is created
-    conf = SomesyConfig(codemeta_file=file_path, cff_file=cff_path)
-    update_codemeta(conf)
-    assert conf.codemeta_file.is_file()
-    dat = open(conf.codemeta_file, "rb").read()
-
-    # second time, no changes -> codemeta.json is the same
-    update_codemeta(conf)
-    dat_2 = open(conf.codemeta_file, "rb").read()
-    assert dat_2 == dat
-
-    cff.description = "Changed description"
-    cff.save()
-    # second time, changes -> codemeta.json is overwritten
-    update_codemeta(conf)
-    dat_3 = open(conf.codemeta_file, "rb").read()
-    assert dat_3 != dat
+    # fourth time, change the project name back and change version in codemeta.json
+    # -> codemeta.json is the same
+    somesy_input.project.name = "testproject"
+    with open(codemeta_file, "w") as f:
+        codemeta = json.loads(dat3)
+        codemeta["version"] = "0.0.2"
+        json.dump(codemeta, f)
+    cm.sync(somesy_input.project)
+    cm.save()
+    assert codemeta_file.is_file()
+    dat4 = open(codemeta_file, "rb").read()
+    assert dat == dat4
+    assert dat3 != dat4
