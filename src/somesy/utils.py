40bc1d0fccfb73ca7be3ddae9c0511169c8ae5e4
Mustafa Soylu <mohkargan@gmail.com>
2024-01-16 10:33:59 +0100

    add json dump wrapper

---
+++ src/somesy/utils.py
@@ -0,0 +1,12 @@
+"""Utility functions for somesy."""
+import wrapt
+
+
+@wrapt.decorator
+def json_dump_wrapper(wrapped, instance, args, kwargs):
+    """Wrap json.dump to write non-ascii characters with default indentation."""
+    # Ensure ensure_ascii is set to False
+    kwargs["ensure_ascii"] = False
+    # set indent to 2 if not set
+    kwargs["indent"] = kwargs.get("indent", 2)
+    return wrapped(*args, **kwargs)
