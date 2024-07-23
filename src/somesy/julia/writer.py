acd3dab6fedb2a44b2ccbc10386746b287966c55
Mustafa Soylu <mohkargan@gmail.com>
2024-07-23 16:01:18 +0200

    disable error raise on users without email in julia

--- src/somesy/julia/writer.py
+++ src/somesy/julia/writer.py
@@ -2,7 +2,7 @@
 
 import logging
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 import tomlkit
 from rich.pretty import pretty_repr
@@ -57,6 +57,52 @@ class Julia(ProjectMetadataWriter):
     def _to_person(person_obj: str) -> Person:
         """Parse name+email string to a Person."""
         return Person.from_name_email_string(person_obj)
+
+    @property
+    def authors(self) -> List[str]:
+        """Get authors from the pyproject.toml file."""
+        # check if authors can be converted to person, only return valid ones
+        authors = self._get_property("authors")
+        if authors is None:
+            return []
+
+        valid_authors = []
+        for author in authors:
+            try:
+                self._to_person(author)
+                valid_authors.append(author)
+            except (ValueError, AttributeError):
+                logger.warning(f"Invalid author format: {author}")
+        return valid_authors
+
+    @authors.setter
+    def authors(self, authors: List[Person]) -> None:
+        """Set the authors of the project."""
+        authors = [self._from_person(c) for c in authors]
+        self._set_property(self._get_key("authors"), authors)
+
+    @property
+    def maintainers(self) -> List[str]:
+        """Get maintainers from the pyproject.toml file."""
+        # check if maintainers can be converted to person, only return valid ones
+        maintainers = self._get_property("maintainers")
+        if maintainers is None:
+            return []
+
+        valid_maintainers = []
+        for maintainer in maintainers:
+            try:
+                self._to_person(maintainer)
+                valid_maintainers.append(maintainer)
+            except (ValueError, AttributeError):
+                logger.warning(f"Invalid maintainer format: {maintainer}")
+        return valid_maintainers
+
+    @maintainers.setter
+    def maintainers(self, maintainers: List[Person]) -> None:
+        """Set the maintainers of the project."""
+        maintainers = [self._from_person(c) for c in maintainers]
+        self._set_property(self._get_key("maintainers"), maintainers)
 
     def sync(self, metadata: ProjectMetadata) -> None:
         """Sync output file with other metadata files."""
