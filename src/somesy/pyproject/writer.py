473dcd31dbec37bcf943e5acf627881d9c08c197
Mustafa Soylu <mohkargan@gmail.com>
2025-02-13 11:02:46 +0100

    make the log info level

--- src/somesy/pyproject/writer.py
+++ src/somesy/pyproject/writer.py
@@ -107,7 +107,7 @@ class Poetry(PyprojectCommon):
         try:
             return Person.from_name_email_string(person)
         except (ValueError, AttributeError):
-            logger.warning(f"Cannot convert {person} to Person object, trying Entity.")
+            logger.info(f"Cannot convert {person} to Person object, trying Entity.")
 
         try:
             return Entity.from_name_email_string(person)
@@ -157,7 +157,7 @@ class SetupTools(PyprojectCommon):
         try:
             return Person.from_name_email_string(person)
         except (ValueError, AttributeError):
-            logger.warning(f"Cannot convert {person} to Person object, trying Entity.")
+            logger.info(f"Cannot convert {person} to Person object, trying Entity.")
 
         try:
             return Entity.from_name_email_string(person)
