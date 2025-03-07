c1c9acecca4846b17ba60721501220121adf7e64
Mustafa Soylu <mohkargan@gmail.com>
2025-03-07 12:15:02 +0100

    fix not existing file not handled

--- src/somesy/pyproject/writer.py
+++ src/somesy/pyproject/writer.py
@@ -65,6 +65,11 @@ class PyprojectCommon(ProjectMetadataWriter):
         """Save the pyproject file."""
         path = path or self.path
 
+        if not path.is_file():
+            with open(path, "w") as f:
+                tomlkit.dump(self._data, f)
+            return
+
         with open(path, "r") as f:
             # tomlkit formatting sometimes creates empty lines, dont change if context is not changed
             existing_data = f.read()
